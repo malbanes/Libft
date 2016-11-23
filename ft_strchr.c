b4f2d9f56b8a59ce0cb89e46d98918d7fd716b99
Morgane ALBANESE <malbanes@e1r3p19.42.fr>
2016-11-23 17:11:11 +0100

    libft

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: malbanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/20 20:07:42 by malbanes          #+#    #+#             */
-/*   Updated: 2016/11/20 20:08:54 by malbanes         ###   ########.fr       */
+/*   Updated: 2016/11/23 17:07:30 by malbanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *str, int c)
 	int i;
 
 	i = 0;
+	if (c == '\0')
+		return ((char*)(str + ft_strlen(str) + 1))
 	while (str[i] != c)
 	{
 		if (str[i] == '\0')
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: malbanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/20 20:09:13 by malbanes          #+#    #+#             */
-/*   Updated: 2016/11/20 20:12:08 by malbanes         ###   ########.fr       */
+/*   Updated: 2016/11/23 17:10:52 by malbanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strrchr(const char *s1, int c)
 	int i;
 
 	i = ft_strlen(s1);
+	if (c == '\0')
+		return ((char*)(s + i + 1));
 	while (s1[i] != c)
 	{
 		if (s1[i] == s1[0])
