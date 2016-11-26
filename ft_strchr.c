1c3161adf619f6ce61ecd33245de616ede556c2f
Morgane ALBANESE <malbanes@e3r10p14.42.fr>
2016-11-26 13:59:51 +0100

    libftfin

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: malbanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/20 20:07:42 by malbanes          #+#    #+#             */
-/*   Updated: 2016/11/26 12:43:20 by malbanes         ###   ########.fr       */
+/*   Updated: 2016/11/26 13:59:10 by malbanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *s, int c)
 	int i;
 
 	i = 0;
+	if ((char)c == '\0')
+		return ((char*)s + ft_strlen(s));
 	while (s[i] != (char)c)
 	{
 		if (s[i] == '\0')
